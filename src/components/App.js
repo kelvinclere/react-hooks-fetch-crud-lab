ac341d1ab531590284ce4dd35ca2c6daae2106c0
kelvinclere <kelvinmutethiamuthomi@gmail.com>
2022-09-28 23:28:52 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([])
+
+    // Fetching Data & Setting it to questions
+    useEffect(()=> {
+      fetch("http://localhost:4000/questions")
+      .then((res) => res.json())
+      .then((quizes) => setQuestions(quizes))
+    },[])
+
+     //Delete Item
+  function handleDeleteItem(deleteItem){
+    const updatedQuestions = questions.filter((question) => question.id !== deleteItem.id);
+    setQuestions(updatedQuestions)
+  }
+
+
+  //function add question
+  function addQuestion(item){
+    const newQuestions = [...questions, item]
+    setQuestions(newQuestions)
+    console.log("added", item)
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm addQuestion={addQuestion}/> : <QuestionList handleDeleteItem={handleDeleteItem}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,13 @@
 import React from "react";
+import QuestionItem  from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({handleDeleteItem, questions}) {
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+      {questions.map((quiz) => <QuestionItem key={quiz.id} question={quiz} handleDeleteItem={handleDeleteItem}/>)}
+      </ul>
     </section>
   );
 }
